f7ff1885df2686b8d3d0a973beaf3b8e7a204e74
Alexa <xla.2@yandex.ru>
2022-06-30 00:09:42 +0300

    Product picture storage and link database has been created item and products templates already use picture links from that database
    delete, add and edit routes work

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template, request, url_for, flash, redirect, abo
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from sqlalchemy import exc
-# from sqlalchemy_imageattach.entity import Image, image_attachment
+
+# file.py has been changed "from collections.abc import Iterable" in sqlalchemy_imageattach!!
+from sqlalchemy_imageattach.entity import Image, image_attachment
+from sqlalchemy_imageattach.stores.fs import HttpExposedFileSystemStore
+from sqlalchemy_imageattach.context import store_context
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
@@ -17,14 +21,16 @@ from werkzeug.utils import secure_filename
 from datetime import date
 import os
 from functools import wraps
+import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzzzz'
 Bootstrap(app)
 
 CATS = ['Merch', 'Household', 'SSS tier']
-cart = {}
-# img_path = None
+
+# cart = {}
+# temp_img_path = None
 
 # file upload folder
 app.config['UPLOAD_FOLDER'] = 'static/temp'
@@ -57,6 +63,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # I have to create this key to use CSRF protection for form
 db = SQLAlchemy(app)
 
+# file storage implementation
+# new folder in path (path/productpics) will be created (and filled) automatically even if it doesn't exist
+hfs_store = HttpExposedFileSystemStore(path='static/',
+                                       prefix='images/',
+                                       host_url_getter=lambda:'http://{0}/'.format('127.0.0.1:5000'))
+# At least for now, only httP, NOT HTTPS as stated in sqlalchemy docs!!!!!
+app.wsgi_app = hfs_store.wsgi_middleware(app.wsgi_app)
+# print(app.config['SERVER_NAME'])
 
 class User(UserMixin, db.Model):
     # Usermixin contains some important methods for our User
@@ -73,10 +87,13 @@ class User(UserMixin, db.Model):
     # avatar = image_attachment('UserPicture')
 
     # each user has some purchased items (list of ids) and a single cart (last one)
+    # delete purchases or db!!!!
     purchases = db.Column(db.PickleType)
     cart = db.Column(db.PickleType)
+
     # 1-Many bi-directional bond
     u_orders = relationship("Order", back_populates="client")
+
     # I just want to return a string with custom printable representation of an object, overrides standard one
     def __repr__(self):
         return f'<User_{self.id}: {self.user_name}>'
@@ -102,19 +119,46 @@ class Product(db.Model):
     p_price = db.Column(db.Integer, nullable=False)
     p_amount = db.Column(db.Integer, nullable=False)
 
-    # p_image = image_attachment('ProductPicture')
+    # 1-1? bi-directional bond
+    p_image = image_attachment('ProductPicture', back_populates="product")
 
-    # I just want to return a string with custom printable representation of an object, overrides standard one
     def __repr__(self):
+        """rrrrrrrepresentacion"""
         return f'<Product_{self.p_id}: {self.p_name}>'
 
+    # implement some kind of storage here
+    def store_to_p_image(self, temp_path):
+        """extracts the file from temp location by path and add/change product's image using that"""
+        with store_context(hfs_store):
+            with open(temp_path, 'rb') as f:
+                self.p_image.from_file(f)
+            db.session.commit()
 
-# class ProductPicture(db.Model, Image):
-#     """User picture model."""
-#     __tablename__ = 'productpics'
-#     product_id = db.Column(db.Integer, db.ForeignKey('products.p_id'), primary_key=True)
-#     product = relationship('Product')
-#     # implement some kind of storage here
+    def upload_p_image(self, f):
+        """unused as I use temp folder for upload in my algorithm"""
+        with store_context(hfs_store):
+            self.p_image.from_file(f)
+            db.session.commit()
+
+    def get_pi_path(self):
+        """get url to the image in storage"""
+        with store_context(hfs_store):
+            # print(self.p_image.locate())
+            return self.p_image.locate()
+
+    def del_pi(self):
+        """I just had to create this one here in order to delete product without errors"""
+        with store_context(hfs_store):
+            self.p_image.delete()
+
+
+class ProductPicture(db.Model, Image):
+    """Product picture model."""
+    __tablename__ = 'productpics'
+    product_id = db.Column(db.Integer, db.ForeignKey('products.p_id'), primary_key=True)
+
+    # 1-1? bi-directional bond
+    product = relationship('Product', back_populates="p_image")
 
 
 @app.before_first_request
@@ -143,6 +187,51 @@ class LoginForm(FlaskForm):
     submit_f = SubmitField('Submit')
 
 
+# {user_id1:[temp_image_path, temp_cart], ...}
+active_session = {}
+# {random user id: cart dict}
+anonymous_session = {}
+anon_ids = [i for i in range(1000)]
+
+
+def update_as(t_path, t_cart={}):
+    """updates cart draft for a particular user"""
+    global active_session, anonymous_session, anon_ids
+    if current_user.is_authenticated:
+        active_session[current_user.id] = [t_path, t_cart]
+    else:
+        # provide random id to each user in order to keep his cart for him
+        try:
+            rand_id = random.choice(anon_ids)
+            anon_ids.remove(rand_id)
+        except IndexError:
+            # what if we run out of 1000 anonymous ids? Let's prevent an excessive server load
+            print('Unable to maintain that many users.')
+
+
+def flush_as(f_cart=False):
+    """deletes cart draft for a particular user"""
+    global active_session
+    active_session[current_user.id][0] = None
+    if f_cart:
+        active_session[current_user.id][1] = {}
+
+
+@app.route("/flush")
+def flush_cart(dbonly=True):
+    """clears any type of cart if that exists"""
+    global active_session
+    # clear session cart = draft for this user, registered or not
+    if not dbonly:
+        flush_as(True)
+    if current_user.is_authenticated:
+        # clear cart for this user (saved in db)
+        current_user.cart = {}
+        db.session.commit()
+        return redirect(url_for('index'))
+    flash('Draft and cart have been reset.', 'info')
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -159,6 +248,8 @@ def login():
             result = db.session.query(User).filter_by(user_name=r_user_name).first()
             if check_password_hash(result.password, r_user_password):
                 login_user(result)
+                # when user logs in, he must be added to active session with initial temp vars
+                update_as(None)
                 flash('You were successfully logged in')
                 return redirect(url_for('index'))
             else:
@@ -174,10 +265,15 @@ def login():
 @app.route('/logout')
 @login_required
 def logout():
-    global cart
-    current_user.cart = cart
-    db.session.commit()
-    cart = {}
+    global active_session
+    try:
+        # remove this user from active session
+        active_session.pop(current_user.id)
+        # but save his cart in db for the next time
+        current_user.cart = active_session[current_user.id][1]
+        db.session.commit()
+    except:
+        print('You forgot to log out.')
     logout_user()
     return redirect(url_for('index'))
 
@@ -199,7 +295,7 @@ def register():
                 db.session.add(new_user)
                 db.session.commit()
                 login_user(new_user)
-                flash(f'{new_user} You were successfully logged in')
+                flash(f'{new_user} You have logged in successfully.')
                 return redirect(url_for('index'))
             except:
                 flash("Couldn't add the user to the database")
@@ -211,55 +307,48 @@ def register():
 @app.route("/products/<int:p_id>")
 @app.route("/products")
 def products(p_id=None):
-    global cart
+    global active_session
+    cart = active_session[current_user.id][1]
     if p_id:
         product = db.session.query(Product).get(p_id)
         return render_template("item.html", pr=product)
     all_products = db.session.query(Product).all()
+    # I use the cart here just to show quantity of each product on page if it is in user's cart
     return render_template("products.html", ap=all_products, cc=cart)
-
-
-@app.route("/flush")
-def flush_cart():
-    """clears any cart if that exists"""
-    global cart
-    cart = {}
-    if current_user.is_authenticated:
-        current_user.cart = {}
-        db.session.commit()
-        return redirect(url_for('index'))
-    flash('Draft and cart have been reset.', 'info')
 
 
 @app.route("/buy/<int:p_id>")
 def buy_pr(p_id):
-    global cart
+    global active_session
+    cart = active_session[current_user.id][1]
     pr_to_buy = db.session.query(Product).get(p_id)
     # I just realised that I can't use sqlalchemy objects as keys, seems they can't be compared or that works wrong way
     # therefore, our cart = {Product object id: [Product object, quantity]}
     try:
+        # try to reach and if this product exists in the cart, update its quantity only
         cart[pr_to_buy.p_id] = [pr_to_buy, cart[pr_to_buy.p_id][1] + 1]
     except KeyError:
         cart[pr_to_buy.p_id] = [pr_to_buy, 1]
     finally:
-        print(cart)
+        update_as(None, cart)
     return redirect(url_for('products'))
 
 
 @app.route("/cart", methods=['GET', 'POST'])
 def show_cart():
-    global cart
-    if current_user.is_authenticated:
-        if cart:
-            # if we have some draft we should update in database
-            current_user.cart = cart
-        else:
-            # if we don't have any draft we should try to retrieve last cart from database
-            cart = current_user.cart
+    global active_session
+    # check not registered user!
+    cart = active_session[current_user.id][1]
+    if current_user.is_authenticated and not cart:
+        # if we don't have any draft we should try to retrieve last cart from database
+        cart = current_user.cart
     if request.method == 'POST':
         act_del1 = request.form.get('del1btn')
         act_conf = request.form.get('scrtbtn')
         if act_del1:
+            if current_user.is_authenticated:
+                # from now on, we are working with draft, we don't need to keep the db cart anymore
+                flush_cart()
             pid_to_del = int(request.form['del1id'])
             item_to_del = cart[pid_to_del]
             if item_to_del[1] == 1:
@@ -271,11 +360,16 @@ def show_cart():
             if current_user.is_authenticated:
                 current_user.cart = cart
                 db.session.commit()
+                # we don't need the cart draft any more
+                flush_as(True)
                 flash('Ready for checkout.', 'info')
                 return redirect(url_for('checkout'))
             else:
                 flash('Please login/register to continue.', 'error')
+                # his cart=draft is going to be deleted on login, thus needs external exception for non-registered users
                 return redirect(url_for('login'))
+        # in case we deleted an item from the cart and came back
+        update_as(None, cart)
     return render_template("cart.html", cc=cart, tot=calc_total(cart) if cart else {})
 
 
@@ -302,11 +396,14 @@ def checkout():
         ex = request.form['cc-expiration']
         cv = request.form['cc-cvv']
         private_data['p'] = {'pame': me, 'ccna': na, 'ccnu': nu, 'ccex': ex, 'cccv': cv}
+
+        # # !!!THIS SILENTLY BREAKS EVERYTHING UNLESS YOU PUT THAT CHECKMARK!!!
         # save all data for future checkmark state
         # save = request.form['save-info']
         # if save:
         #     current_user.private_details = private_data
         #     db.session.commit()
+
         # final_cart = {Product object id: [Product object, quantity], ...}
         # I don't want to store whole product objects in orders as they could mutate,
         # contents = [{Product object id: quantity, ...}, total]
@@ -314,6 +411,8 @@ def checkout():
         db.session.add(new_order)
         db.session.commit()
         flash('Your order has been registered', 'info')
+        # remove existing cart from database
+        flush_cart()
         return redirect(url_for('my_orders'))
     return render_template("checkout.html", fc=final_cart, tot=total)
 
@@ -345,8 +444,10 @@ def control_panel():
 @login_required
 @admin_only
 def add_product():
-    global img_path
+    global active_session
     if request.method == 'POST':
+        # we should have an image path when we upload and come back via POST
+        temp_img_path = active_session[1][0]
         f = request.files.get('file')
         ti_add = request.form.get('addbtn')
 
@@ -362,28 +463,38 @@ def add_product():
                               p_amount=p_amo)
         # add text input for product
         if ti_add:
-            try:
-                db.session.add(new_product)
-                db.session.commit()
-                flash(f'Product with the name {p_name} has been successfully added.', 'info')
-                return redirect(url_for('products'))
-            except exc.IntegrityError:
-                flash(f'Product with the name {p_name} already exists in the database.', 'error')
+            if temp_img_path:
+                try:
+                    db.session.add(new_product)
+                    db.session.commit()
+                    db.session.refresh(new_product)
+                    new_product.store_to_p_image(temp_img_path)
+                    # print(new_product.get_pi_path())
+                    # success => we don't need an img path any more
+                    flush_as()
+                    flash(f'Product with the name {p_name} has been successfully added.', 'info')
+                    return redirect(url_for('products'))
+                except exc.IntegrityError:
+                    flash(f'Product with the name {p_name} already exists in the database.', 'error')
+            else:
+                flash('Add an image for that product', 'error')
+                return render_template('add.html', pr=new_product, cat_list=CATS)
         # image upload handler section
         elif f:
             if f.filename != '':
                 if is_allowed(f.filename):
-                    img_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename))
-                    f.save(img_path)
+                    temp_img_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename))
+                    f.save(temp_img_path)
+                    # as we upload something, we should add the path to our active session too
+                    update_as(temp_img_path)
                     flash('file uploaded successfully', 'info')
-                    return render_template('add.html', pr_image=img_path, pr=new_product, cat_list=CATS)
+                    return render_template('add.html', pr_image=temp_img_path, pr=new_product, cat_list=CATS)
                 else:
                     flash('incorrect file', 'error')
             else:
                 flash('No selected file', 'error')
-
-        else:
-            flash('Add an image for that product', 'error')
+    # when we GET this url without POST we should take care of possibly leftover draft
+    flush_as()
     return render_template("add.html", pr=None, cat_list=CATS)
 
 
@@ -391,9 +502,11 @@ def add_product():
 @login_required
 @admin_only
 def edit_pr(p_id):
+    global active_session
     pr_to_upd = db.session.query(Product).get(p_id)
     if request.method == 'POST':
-        # f = request.files.get('file')
+        # we MAY have an image path when we upload and come back via POST
+        temp_img_path = active_session[1][0]
         # renew text input for product
         f = request.files.get('file')
         ti_conf = request.form.get('confbtn')
@@ -413,6 +526,12 @@ def edit_pr(p_id):
                 pr_to_upd.p_amount = temp_product.p_amount
                 db.session.commit()
                 flash(f'Product with the name {pr_to_upd.p_name} has been successfully updated.', 'info')
+                if temp_img_path:
+                    db.session.refresh(pr_to_upd)
+                    pr_to_upd.store_to_p_image(temp_img_path)
+                    flash("Product's image has been changed.", 'info')
+                else:
+                    flash("Product's image has left unchanged.", 'info')
                 return redirect(url_for('products'))
             except exc.IntegrityError:
                 flash(f'Product with the name {pr_to_upd.p_name} already exists in the database.', 'error')
@@ -420,17 +539,21 @@ def edit_pr(p_id):
         elif f:
             if f.filename != '':
                 if is_allowed(f.filename):
-                    img_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename))
-                    f.save(img_path)
+                    temp_img_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename))
+                    # print(secure_filename(f.filename), app.config['UPLOAD_FOLDER'], temp_img_path)
+                    f.save(temp_img_path)
+                    # as we upload something, we should add the path to our active session too
+                    update_as(temp_img_path)
                     flash('file uploaded successfully', 'info')
-                    return render_template('add.html', pr_image=img_path, pr=temp_product, cat_list=CATS)
+                    # '/' must be here otherwise it adds /edit/ and results in error
+                    return render_template('add.html', pr_image='/'+temp_img_path, pr=temp_product, ed=True, cat_list=CATS)
                 else:
                     flash('incorrect file', 'error')
             else:
                 flash('No selected file', 'error')
-        # else:
-        #     flash('Add an image for that product', 'error')
-    return render_template("add.html", pr=pr_to_upd, ed=True, cat_list=CATS)
+    # when we GET this url without POST we should take care of possibly leftover draft
+    flush_as()
+    return render_template("add.html", pr_image=pr_to_upd.get_pi_path(), pr=pr_to_upd, ed=True, cat_list=CATS)
 
 
 @app.route("/delete/<int:p_id>")
@@ -438,6 +561,7 @@ def edit_pr(p_id):
 @admin_only
 def del_pr(p_id):
     pr_to_del = db.session.query(Product).get(p_id)
+    pr_to_del.del_pi()
     db.session.delete(pr_to_del)
     db.session.commit()
     flash(f'Product with the name {pr_to_del.p_name} has been successfully deleted.', "info")
